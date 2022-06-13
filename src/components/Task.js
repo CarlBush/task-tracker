1db6cdab9b0d0ec40c1e6c1922b9228cf0e4cfeb
Carl Bush <CarlBushs13@gmail.com>
2022-06-13 12:51:35 -0700

    delete task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,15 @@
 import { FaTimes } from "react-icons/fa"
 //npm install react-icons
 
-function Task( {task} ) {
+function Task({ task, onDelete }) {
     return (
         <div className="task">
-            <h3>{task.text} <FaTimes style={{color: "red", cursor: "pointer" }}/></h3>
+            <h3>{task.text}
+                <FaTimes
+                    style={{ color: "red", cursor: "pointer" }}
+                    onClick={() => onDelete(task.id)}
+                />
+            </h3>
             <p>{task.day}</p>
         </div>
     )
